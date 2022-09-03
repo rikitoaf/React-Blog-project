f445d4b5de783a3a0086bfe57024c7d6efccc663
AF Rakib <38295735+rikitoaf@users.noreply.github.com>
2022-09-03 14:47:22 +0600

    Final Version

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -9,12 +13,27 @@ function App() {
   // const likes = 50;
   // const person = {name : 'yoshi', age : '25'};
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div class="content">
-      <Home></Home>
+      <Switch>
+        <Route exact path="/">
+        <Home/>
+        </Route>
+        <Route path="/create">
+         <Create/>
+        </Route>
+        <Route path="/blogs/:id">
+          <BlogDetails/>
+        </Route>
+        <Route path = "*">
+          <NotFound />
+        </Route>
+      </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,16 @@
-import { useState, useEffect } from "react";
+
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () =>{
 
-    // let name = 'mario';
-   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-   ]);
-
-   const [name, setName] = useState('mario');
-
-   const handleDelete = (id) => {
-       const newBlogs = blogs.filter(blog => blog.id!== id);
-       setBlogs(newBlogs);
-   }
-
-   useEffect(()=>{
-    console.log('use effect ran');
-    console.log(name);
-   }, [name]);
+    const {data: blogs,isPending,error} = useFetch('http://localhost:8000/blogs');
+ 
     return (
         <div className="home">
-           <BlogList blogs = {blogs} title = "All blog!" handleDelete= {handleDelete}/>
-           {/* <BlogList blogs = {blogs.filter((blog) => blog.author === 'mario')} title = "Mario's blog!"/> */}
-            <button onClick={() => setName('luigi')}>Change Name</button>
-            <p>{name}</p>
+            {error && <div> {error} </div> }
+            {isPending && <div>Loading...</div>}
+           {blogs && <BlogList blogs = {blogs} title = "All blog!" />}
         </div>
     );
 }
